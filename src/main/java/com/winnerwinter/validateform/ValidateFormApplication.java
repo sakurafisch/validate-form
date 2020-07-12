52c892c5a0dc066a53e42ac1453120e4bd8aaae7
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-07-12 21:27:42 +0800

    Basically complete!

---
+++ src/main/java/com/winnerwinter/validateform/ValidateFormApplication.java
@@ -0,0 +1,13 @@
+package com.winnerwinter.validateform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateFormApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ValidateFormApplication.class, args);
+	}
+
+}
